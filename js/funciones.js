f233fe43e193add666548c66f71aa46c80352320
teleiosjad <104934667+teleiosjad@users.noreply.github.com>
2022-05-05 20:54:23 -0400

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -15,17 +15,17 @@ function posicionarMenu() {
         $('.logo-blanco').css('opacity', '0').css('transition', '0.5s').css('margin-top', '15px');
         $('.logo-normal').css('opacity', '1').css('transition', '0.5s').css('margin-top', '15px');
         $('.nav-bar').css('background-color', 'white').css('transition', '0.5s').css('border-bottom', '1px solid rgba(128, 128, 128, 0.2)').css('height', '80px');
-        $('nav ul.menu-pc').css('margin-top', '16px').css('color', 'black').css('transition', '0.5s');
+        $('nav ul.menu-pc').css('margin-top', '26px').css('color', 'black').css('transition', '0.5s');
         $('nav ul.menu-pc a').css('color', 'black').css('transition', '0.5s');
-        $('.icon-menu').css('color', 'black').css('padding-top', '24px').css('transition', '0.5s');
+        $('.icon-menu').css('color', 'black').css('padding-top', '30px').css('transition', '0.5s');
        } else {
         // $('.menu').removeClass('fixed');
         $('.logo-blanco').css('opacity', '1').css('transition', '0.5s').css('margin-top', '42px');
         $('.logo-normal').css('opacity', '0').css('transition', '0.5s').css('margin-top', '42px');
         $('.nav-bar').css('background-color', 'rgba(255, 255, 255, 0)').css('transition', '0.5s').css('border-bottom', 'none');
-        $('nav ul.menu-pc').css('margin-top', '42px').css('transition', '0.5s');
+        $('nav ul.menu-pc').css('margin-top', '26px').css('transition', '0.5s');
         $('nav ul.menu-pc a').css('color', 'white').css('transition', '0.5s');
-        $('.icon-menu').css('color', 'white').css('padding-top', '49px').css('transition', '0.5s');
+        $('.icon-menu').css('color', 'white').css('padding-top', '30px').css('transition', '0.5s');
         
     }
 }
